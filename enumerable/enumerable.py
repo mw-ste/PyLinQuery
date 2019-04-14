c898b994eb6801f0df751a28da5d3ee068d27fda
Michael Stenger <michael.stenger@outlook.de>
2019-04-14 12:42:21 +0200

    adding basic implementation and unittests

---
+++ enumerable/enumerable.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 11 15:51:21 2019
+
+@author: michi
+"""
+
+import inspect
+from types import FunctionType
+
+# In[]
+class Enumerable:
+    _generator = None
+    _values    = None
+    
+    
+    # In[]
+    def __init__(self, values):
+        if Enumerable._is_generator_type(values):
+            self._generator = values
+        elif Enumerable._is_list_type(values):
+            self._generator = Enumerable._create_generator(values)
+        else:
+            error  = "Values need to be of type generator function, list or tuple, but was: "
+            error += str(type(values))
+            raise TypeError(error)
+
+
+    # In[]
+    def to_list(self):
+        if self._values == None:
+            self._values = list(self._generator())
+        return self._values
+
+
+    # In[]
+    def where(self, method):
+        Enumerable._check_is_function(method)
+        def generate():
+            for element in self._get_values():
+                if(method(element)):
+                    yield element
+        return Enumerable(generate)
+
+
+    # In[]
+    def select(self, method):
+        Enumerable._check_is_function(method)
+        def generate():
+            for element in self._get_values():
+                yield method(element)
+        return Enumerable(generate)
+
+
+    # In[]
+    def _get_values(self):
+        return self._generator()
+
+
+    # In[]
+    def _check_is_function(method):
+        if not Enumerable._is_function_type(method):
+            error  = "Method need to be of type function, but was: "
+            error += str(type(method))
+            raise TypeError(error)
+
+
+    # In[]
+    def _create_generator(elements):
+        def generate():
+            for element in elements:
+                yield element
+        return generate
+
+
+    # In[]
+    def _is_function_type(element):
+        generator_types = [FunctionType]
+        return Enumerable._check_type(element, generator_types)
+
+
+    # In[]
+    def _is_generator_type(element):
+        return inspect.isgeneratorfunction(element)
+
+    
+    # In[]    
+    def _is_list_type(element):
+        list_types = [list, tuple]
+        return Enumerable._check_type(element, list_types)
+
+    
+    # In[]    
+    def _check_type(element, types):
+        is_type_instance = [isinstance(element, current_type) for current_type in types]
+        is_any_element_of_type = sum(is_type_instance) > 0
+        return is_any_element_of_type
---
+++ enumerable/enumerable_tests.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 10:22:06 2019
+
+@author: Michi
+"""
+
+from enumerable import Enumerable
+import unittest
+
+
+# In[]
+def generate():
+    for i in range(8):
+        yield i
+
+
+# In[]
+class Dummy:
+    A = None
+    B = None
+    
+    def __init__(self, A = 0, B = 0):
+        self.A = A
+        self.B = B
+
+
+# In[]
+class EnumerableTests(unittest.TestCase):
+
+    # In[]
+    def test_when_create_enumerable_with_list_then_create_valid_enumerable(self):
+        self.assertIsNotNone(Enumerable([1, 2, 3]))
+        self.assertIsNotNone(Enumerable(["a", "b", "c"]))
+
+
+    # In[]
+    def test_when_create_enumerable_with_tuple_then_create_valid_enumerable(self):
+        self.assertIsNotNone(Enumerable((1, 2, 3)))
+        self.assertIsNotNone(Enumerable(("a", "b", "c")))
+
+
+    # In[]
+    def test_when_create_enumerable_with_generator_function_then_create_valid_enumerable(self):
+        self.assertIsNotNone(Enumerable(generate))
+
+
+    # In[]
+    def test_when_create_enumerable_with_generator_then_raise_type_error(self):
+        with self.assertRaises(TypeError):
+            Enumerable(generate())
+
+
+    # In[]
+    def test_when_create_enumerable_with_dictionary_then_raise_type_error(self):
+        with self.assertRaises(TypeError):
+            Enumerable({"1" : 1, "2" : 2, "3" : 3})
+
+
+    # In[]
+    def test_when_create_enumerable_with_string_then_raise_type_error(self):
+        with self.assertRaises(TypeError):
+            Enumerable("test")
+
+
+    # In[]
+    def test_when_run_where_then_return_correct_values(self):
+        enumerable = Enumerable(generate)
+        
+        where_enumerable = enumerable.where(lambda x: x > 2)
+        self.assertIsInstance(where_enumerable, Enumerable)
+        
+        values = where_enumerable.to_list()
+        self.assertSequenceEqual(values, [3, 4, 5, 6, 7])
+
+
+    # In[]
+    def test_when_run_several_where_in_sequence_then_return_correct_values(self):
+        enumerable = Enumerable(generate)
+        
+        where_enumerable = enumerable.where(lambda x: x > 2).where(lambda x: x < 6)
+        self.assertIsInstance(where_enumerable, Enumerable)
+        
+        values = where_enumerable.to_list()
+        self.assertSequenceEqual(values, [3, 4, 5])
+
+
+    # In[]
+    def test_when_run_where_then_enumerate_only_after_to_list(self):
+        sequence = []
+        
+        def less_than_5(x):
+            w = x < 5
+            sequence.append(5)
+            return w
+        
+        enumerable = Enumerable(generate).where(less_than_5).where(less_than_5)
+        self.assertTrue(len(sequence) == 0)
+        
+        enumerable.to_list()
+        self.assertTrue(len(sequence) > 0)
+
+
+    # In[]
+    def test_when_run_to_list_several_times_then_enumerate_only_once(self):
+        sequence = []
+        
+        def less_than_5(x):
+            w = x < 5
+            sequence.append(5)
+            return w
+        
+        enumerable = Enumerable(generate).where(less_than_5)
+        
+        enumerable.to_list()
+        counter = len(sequence)
+        
+        enumerable.to_list()
+        self.assertEqual(counter, len(sequence))
+
+
+    # In[]
+    def test_when_run_several_where_in_sequence_then_run_interleaved(self):
+        sequence = []
+        
+        def less_than_5(x):
+            w = x < 5
+            sequence.append(5)
+            return w
+        
+        def greater_than_2(x):
+            w = x > 2
+            sequence.append(2)
+            return w
+        
+        enumerable = Enumerable(generate)
+        values = enumerable.where(greater_than_2).where(less_than_5).to_list()
+        
+        self.assertSequenceEqual(values, [3, 4])
+        self.assertSequenceEqual(sequence, [2, 2, 2, 2, 5, 2, 5, 2, 5, 2, 5, 2, 5])
+
+
+    # In[]
+    def test_when_create_several_enumerables_from_same_enumerable_then_return_correct_values(self):
+        enumerable_1 = Enumerable(generate).where(lambda x: x > 0)
+        
+        enumerable_2 = enumerable_1.where(lambda x: x > 4)
+        enumerable_3 = enumerable_1.where(lambda x: x < 4)
+        
+        self.assertSequenceEqual(enumerable_3.to_list(), [1, 2, 3])
+        self.assertSequenceEqual(enumerable_2.to_list(), [5, 6, 7])
+
+
+    # In[]
+    def test_when_run_select_then_return_correct_values(self):
+        dummies = [Dummy(1, 2), Dummy(2, 4), Dummy(3, 6), Dummy(4, 8)]
+        
+        values = Enumerable(dummies).select(lambda x: x.A).to_list()
+        self.assertSequenceEqual(values, [1, 2, 3, 4])
+        
+        values = Enumerable(dummies).select(lambda x: x.B).to_list()
+        self.assertSequenceEqual(values, [2, 4, 6, 8])
+
+
+    # In[]
+    def test_when_run_where_and_select_in_sequence_then_return_correct_values(self):
+        dummies = [Dummy(1, 2), Dummy(2, 4), Dummy(3, 6), Dummy(4, 8)]
+        
+        values = Enumerable(dummies).where(lambda x: x.A < 3).select(lambda x: x.B).to_list()
+        self.assertSequenceEqual(values, [2, 4])  
+
+
+# In[]
+if __name__ == '__main__':
+    unittest.main()
+    
